810ee349bd274fafa17167d86c9203c82c8de979
willi6m7 <willi6m7@mail.uc.edu>
2022-11-30 20:21:39 -0500

    initial files

---
+++ game_folder/MainMenu.js
@@ -0,0 +1,13 @@
+class MainMenu extends Phaser.Scene(){
+    constructor(){
+        super("bootGame");
+    }
+
+    preload(){
+        //load images, spritesheets, etc
+    }
+
+    create(){
+        //create anims, scene items, etc.
+    }
+}
---
+++ game_folder/game.js
@@ -0,0 +1,25 @@
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 640,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: {
+        preload,
+        create,
+        update
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: false
+        }
+    }
+};
+
+const game = new Phaser.Game(config);
+
+//variables go here (let x = y, etc.)
