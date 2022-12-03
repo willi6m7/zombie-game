6c3c3e0a54b677552453009fe47693afdcd52c23
willi6m7 <willi6m7@mail.uc.edu>
2022-12-03 15:28:26 -0500

    add dummy data for ammo and score

--- game_folder/Scene1.js
+++ game_folder/Scene1.js
@@ -4,7 +4,19 @@ class Scene1 extends Phaser.Scene{
     }
 
     create(){
-        this.add.text(0, 0, 'Started Game!');
-        //most of the game functionality will be here
+        this.add.text(0, 259, 'Started Game!');
+
+        const ammoScoreCounter = this.add.image(10, 10, 'status');
+        const ammoScoreCounterText = this.add.text(0, 0, '');
+
+        ammoScoreCounter.setDataEnabled();
+
+        ammoScoreCounter.data.set('ammo', 200);
+        ammoScoreCounter.data.set('score', 0);
+        
+        ammoScoreCounterText.setText([
+            'Ammo: ' + ammoScoreCounter.data.get('ammo'),
+            'Score: ' + ammoScoreCounter.data.get('score')
+        ]);
     }
 }
