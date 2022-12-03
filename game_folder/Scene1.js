d8da798964eb05617dac08e035c8f400e44a6ce1
willi6m7 <willi6m7@mail.uc.edu>
2022-12-03 15:14:57 -0500

    bug fixes

--- game_folder/Scene1.js
+++ game_folder/Scene1.js
@@ -1,9 +1,10 @@
-class Scene2 extends Phaser.Scene{
+class Scene1 extends Phaser.Scene{
     constructor(){
         super("playGame");
     }
 
     create(){
+        this.add.text(0, 0, 'Started Game!');
         //most of the game functionality will be here
     }
 }
